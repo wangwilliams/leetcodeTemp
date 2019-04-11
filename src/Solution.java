51eb0458229dab44f8937528380cc3ca93523393
WangWilliams <1179574672@qq.com>
2019-04-11 14:55:15 +0800

    first

--- src/Solution.java
+++ src/Solution.java
@@ -4,6 +4,7 @@ public class Solution {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String temp = sc.nextLine();
-
+        System.out.println(temp);
+        System.out.println("end");
     }
 }

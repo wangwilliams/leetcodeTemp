83f700633e0063704d321a5001b1fe0d9fc011b7
WangWilliams <1179574672@qq.com>
2019-04-15 23:37:43 +0800

    first

--- src/_282ExpressionAddOperators/Solution.java
+++ src/_282ExpressionAddOperators/Solution.java
@@ -2,17 +2,39 @@ package _282ExpressionAddOperators;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String[] presorder = sc.nextLine().split(" ");
         int target = Integer.parseInt(presorder[0]);
-        int[] me = SplitNum(target);
-        System.out.println(me[1]+" "+me[0]);
+        //int[] me = SplitNum(target);
+        //System.out.println(me[1]+" "+me[0]);
+        presorder = sc.nextLine().split(" ");
+        ListNode listNode = new ListNode(Integer.parseInt(presorder[0]));
+        ListNode head = listNode;
+        for (int i=1;i<target;i++){
+            listNode.next = new ListNode(Integer.parseInt(presorder[i]));
+            listNode = listNode.next;
+        }
+        System.out.println(hasCycle(head));
     }
 
+    public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+
+    }
+    public static boolean hasCycle(ListNode head) {
+        Set<ListNode> settemp = new HashSet<>();
+        while (head.next != null) {
+            if (settemp.contains(head)){
+                return true;
+            }
+            settemp.add(head);
+            head = head.next;
+        }
+        return false;
+    }
     private static int[] SplitNum(int num){
         for (int i=2;i<num;i++){
             if (num%i == 0){
@@ -32,4 +54,12 @@ public class Solution {
     private static void splitArray(String num,List<List<Integer>> ans,int rightSide,int KnifeCount){
 
     }
+}
+class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+         val = x;
+         next = null;
+      }
 }
